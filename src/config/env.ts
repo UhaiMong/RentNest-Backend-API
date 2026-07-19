4886fc8b9f1830331f674706f678220181f831e2
UhaiMong <uhaimarma2016@gmail.com>
2026-07-19 10:39:35 +0600

    modefied config/env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,14 +3,14 @@ dotenv.config();
 
 export const env = {
   port: process.env.PORT || 5000,
-  database_url: process.env.DATABASE_URL as string,
-  app_url: process.env.APP_URL,
-  node_env: process.env.NODE_ENV as string,
-  jwt_secret: process.env.JWT_SECRET as string,
-  jwt_expires_in: process.env.JWT_EXPIRES_IN as string,
-  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
-  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string,
+  database_url: process.env.DATABASE_URL || "",
+  app_url: process.env.APP_URL || "",
+  node_env: process.env.NODE_ENV || "development",
+  jwt_secret: process.env.JWT_SECRET || "",
+  jwt_expires_in: process.env.JWT_EXPIRES_IN || "7d",
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || "",
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN || "30d",
   bcrypt_salt_rounds: parseInt(process.env.BCRYPT_SALT_ROUNDS ?? "10", 10),
-  strip_secret_key: process.env.STRIPE_SECRET_KEY! as string,
-  strip_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET! as string,
+  strip_secret_key: process.env.STRIPE_SECRET_KEY || "",
+  strip_webhook_secret: process.env.STRIPE_WEBHOOK_SECRET || "",
 };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,8 @@
-// import { defineConfig } from "tsup";
-
-// export default defineConfig({
-//   entry: ["src/server.ts"],
-//   outDir: "dist",
-//   format: ["esm"],
-//   target: "node20",
-//   platform: "node",
-//   sourcemap: true,
-//   clean: true,
-//   splitting: false,
-//   bundle: false,
-//   dts: false,
-//   shims: false,
-// });
-
 import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
-  format: ["esm"], // Keep this as ESM
+  format: ["esm"],
   target: "esnext",
   outDir: "dist",
   clean: true,
